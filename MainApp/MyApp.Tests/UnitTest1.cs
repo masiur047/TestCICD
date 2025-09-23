6128b1fafa1e84bdb1d9f8da7c9015d16e70f841
mouli afrin proma <mouliafrinproma100@gmail.com>
2025-09-23 07:19:33 +0600

    Initial commit

---
+++ MainApp/MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace MyApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Addition_Works()
+    {
+        int a = 2, b = 3;
+        int result = a + b;
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Failing_Test()
+    {
+        int x = 10, y = 5;
+        Assert.Equal(20, x + y); // ❌ This will fail
+    }
+}
